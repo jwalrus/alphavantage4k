afe1ddef3f824d3993309547df1128e2ba5982f9
jwalrus <jwalrus@protonmail.com>
2021-02-07 21:33:01 -0500

    playing with api

--- alphavantage4k-http/src/test/kotlin/com/jwalrus/alphavantage4k/http/GetTest.kt
+++ alphavantage4k-http/src/test/kotlin/com/jwalrus/alphavantage4k/http/GetTest.kt
@@ -1,25 +1,27 @@
 package com.jwalrus.alphavantage4k.http
 
 import com.jwalrus.alphavantage4k.http.enums.OutputSize
-import com.jwalrus.alphavantage4k.http.stock.DailyAdjusted
-import com.jwalrus.alphavantage4k.http.stock.Quote
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
+import java.nio.file.Paths
 
 
 class GetUtil : DescribeSpec({
 
-    val testSubject = DailyAdjusted()
+    val apiKey = Paths.get("src", "test", "resources", "api-key.txt").toFile().readText()
+    val client = AlphaVantageClient(apiKey)
 
     describe("daily adjusted returns") {
-        val series = testSubject.fetch("F", OutputSize.LAST_100).also(::println)
+//        val series = DailyAdjusted().fetch("F", OutputSize.LAST_100).also(::println)
+        val series = client.dailyAdjusted("F", OutputSize.LAST_100)
         series.metadata.symbol shouldBe "F"
         series.timeSeries.size shouldBeExactly 100
     }
 
     describe("global quote") {
-        val quote = Quote().fetch("F").also(::println)
+//        val quote = Quote().fetch("F").also(::println)
+        val quote = client.quote("F")
         quote.symbol shouldBe "F"
     }
 })
